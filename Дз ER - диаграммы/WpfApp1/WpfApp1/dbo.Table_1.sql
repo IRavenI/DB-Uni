d1f9c34ab73be18dbff8231b1fa730ff2b68fea8
RaitoOni <41661474+IRavenI@users.noreply.github.com>
2022-09-10 12:01:46 +0800

    Добавьте файлы проекта.

---
+++ Дз ER - диаграммы/WpfApp1/WpfApp1/dbo.Table_1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Регистрация_продаж]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	Код_покупателя int foreign key references dbo.Покупатели(ID_Покупателя) NOT NULL,
+	Код_книги int foreign key references dbo.Книги([ID_Книги]) NOT NULL,
+	Код_продавца int foreign key references dbo.Продавцы(ID_Продавца) NOT NULL, 
+    [Дата_покупки] DATE NOT NULL,
+)
